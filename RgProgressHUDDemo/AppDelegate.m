916a2814c56172e1482da1ba05d7d9ba3d69ce19
RogueAndy <zlj_1969_love@126.com>
2021-12-28 14:18:00 +0800

    no message

--- RgProgressHUDDemo/AppDelegate.m
+++ RgProgressHUDDemo/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launc
+    // Override point for customization after application laun
     return YES;
 }
 
